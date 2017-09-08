fa4d55e47e03146fa42f3a0598798a155906bd24
java <java@bigdata>
2017-09-08 14:37:55 +0900

    first commit (config) pom.xml 	1. java compiler version 1.5 => 1.8 	2. scala-maven-plugin && scala-libray 	3. lombok 	4. commons-logging && log4j 	5. dbcp2 && mysql jdbc driver 	6. spring-context 	7. spring-test && junit4

---
+++ src/main/scala/com/example/scala/SpringDIExample.scala
@@ -0,0 +1,15 @@
+package com.example.scala
+
+import org.springframework.context.support.GenericXmlApplicationContext
+
+object SpringDIExample {
+  	
+	def main(args: Array[String]): Unit = {
+	  
+		var ctx = new GenericXmlApplicationContext("classpath:/spring/beans.xml");
+		
+//		var dept =ctx.getBean(new Dept().getClass)
+		
+		ctx.close()
+	}
+}
